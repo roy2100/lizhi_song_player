f648238f5a695a0ae237080d528c670751c1c5c3
nanlei <36439285+roy2100@users.noreply.github.com>
2026-05-13 23:14:46 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+  },
 });
